bbfbfc74a9a012cfa5dc1c557392fc43c33bbab0
kkamara <kamaracomputers@gmail.com>
2025-03-01 19:00:07 +0000

    Design storage screen & add save & load functionality

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,14 +1,74 @@
-import { StyleSheet } from 'react-native';
+import React, { useState, } from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Button,
+} from 'react-native';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import storage from '@/storage';
+import { STORAGE_KEY, } from '@/constants/Storage';
 
 export default function TabOneScreen() {
+  const [input, setInput] = useState("");
+  const [text, setText] = useState("");
+
+  const saveTextToStorage = async (text: string) => {
+    try {
+      await storage.save({
+        key: STORAGE_KEY,
+        data: { text, },
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  
+  const loadTextFromStorage = async (text: string) => {
+    await storage.load({
+      key: STORAGE_KEY,
+      autoSync: true,
+      syncInBackground: false,
+    })
+      .then(ret => {
+        setText(ret.text);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  };
+
+  const onSaveButtonPress = async () => {
+    await saveTextToStorage(input);
+  };
+
+  const onLoadButtonPress = async () => {
+    await loadTextFromStorage(input);
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <View style={styles.saveView}>
+        <Text>Insert text:</Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={setInput}
+          value={input}
+        />
+        <Button 
+          title="Save"
+          onPress={onSaveButtonPress}
+        />
+      </View>
+      <View style={styles.loadView}>
+        <Button
+          title="Load"
+          onPress={onLoadButtonPress}
+        />
+        {text && <Text>{text}</Text>}
+      </View>
     </View>
   );
 }
@@ -23,9 +83,22 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#000",
+    width: 350,
+    borderRadius: 2,
+  },
+  saveView: {
+    borderWidth: 1,
+    borderColor: "#000",
+    padding: 20,
+    borderRadius: 10,
+  },
+  loadView: {
+    borderWidth: 1,
+    borderColor: "#000",
+    padding: 20,
+    borderRadius: 10,
   },
 });
